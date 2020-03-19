d832b837948616659c4f15ba191d30a3f68216b8
Jan-Felix Abellera <jfabellera@gmail.com>
2020-03-19 02:44:37 -0500

    create project and add splash screen

---
+++ app/src/main/java/com/example/slim_walking/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.slim_walking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    Timer timer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, homepage.class);
+                startActivity(intent);
+            }
+        }, 3000);
+    }
+}
