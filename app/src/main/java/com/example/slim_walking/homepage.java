04b9e4db72b44a4c79d9c8b0d14a2d885d3951cc
Jan-Felix Abellera <jfabellera@gmail.com>
2020-03-19 04:48:32 -0500

    add speech to text for destination input

--- app/src/main/java/com/example/slim_walking/homepage.java
+++ app/src/main/java/com/example/slim_walking/homepage.java
@@ -1,31 +1,41 @@
 package com.example.slim_walking;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import org.w3c.dom.Text;
 
 import java.sql.Time;
+import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class homepage extends AppCompatActivity {
     private TextToSpeech tts;
-
+    private static final int RECOGNIZER_RESULT = 1;
+    ImageButton inputButton;
+    EditText dst;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        inputButton = findViewById(R.id.inputButton);
+        dst = findViewById(R.id.dst);
 
         // initialize text-to-speech variable
         tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
@@ -45,9 +55,6 @@ public class homepage extends AppCompatActivity {
             }
         });
 
-
-
-        ImageButton inputButton = findViewById(R.id.inputButton);
         inputButton.setOnTouchListener(new View.OnTouchListener() {
             Timer t;
             boolean held;
@@ -56,21 +63,28 @@ public class homepage extends AppCompatActivity {
             public boolean onTouch(View v, MotionEvent event) {
                 switch(event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
+                        inputButton.setAlpha(0.85f);
                         //start timer
                         t = new Timer();
                         held = false;
                         t.schedule(new TimerTask() {
                             @Override
                             public void run() {
-                                System.out.println("held");
+                                speak(""); // cancel tts if tts is running
+                                Intent speechIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                                speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                                speechIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say your destination");
+                                startActivityForResult(speechIntent, RECOGNIZER_RESULT);
+
                                 held = true;
                             }
                         }, 1000); //time out 5s
                         return true;
                     case MotionEvent.ACTION_UP:
+                        inputButton.setAlpha(1f);
                         t.cancel();
                         if(!held) {
-                            speak("Hold button to enter destination. Triple tap to hear instructions.");
+                            speak("Hold button to enter destination. Double tap to hear instructions.");
                         }
                         return true;
                 }
@@ -81,6 +95,8 @@ public class homepage extends AppCompatActivity {
 
 
     }
+
+    // text-to-speech function
     private void speak(String text) {
         tts.setPitch(1);
         tts.setSpeechRate(1);
@@ -94,5 +110,14 @@ public class homepage extends AppCompatActivity {
             tts.shutdown();
         }
         super.onDestroy();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == RECOGNIZER_RESULT && resultCode == RESULT_OK) {
+            ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            dst.setText(matches.get(0).toString());
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
